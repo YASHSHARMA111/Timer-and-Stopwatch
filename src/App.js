29db9154969dfd5e9a876d05df4c4ad5f3483a78
yashsharma9295 <yash.sharma9295@gmail.com>
2024-03-03 05:26:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./styles.css";
+import Timer from "./Timer";
+import Stopwatch from "./Stopwatch";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="mt-5 mb-3 text-center">Timer and Stopwatch</h1>
+      <p className="mt-5 mb-3 text-center">Made with ❤️ by Yash Sharma</p>
+      <div className="row">
+        <div className="col-md-6">
+          <Timer />
+        </div>
+        <div className="col-md-6">
+          <Stopwatch />
+        </div>
+        <footer className="footer text-center">
+          <p>&copy; 2023 Yash Sharma. All rights reserved.</p>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default App;
